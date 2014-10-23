14d34014144f27ec2b4dfec1d87f98943465db77
poyopaya <poyopaya@users.noreply.github.com>
2014-10-23 13:10:16 +0200

    Update users.php

--- auth/users.php
+++ auth/users.php
@@ -4,16 +4,16 @@
 // - add an encryption method
 -->
 <?php
-$users = [
-	"test" => "test",
+$users = [					//create $users-array (PHP5.4)
+	"test" => "test",			// structure: "username" => "password",
 	"user2" => "user2_password",
 	"user3" => "user3_password",
 	"user4" => "user4_password",
 ];
-function check($username, $password){
-	if ($password === $users[$username]){
-		return true;
-	}else {
+function check($username, $password){		// method for checking if parametrisized password and username are found in array
+	if ($password === $users[$username]){	// if password saved under array("username"-> "password") equas with submitted password,
+		return true;			//return true
+	}else {					//else return false
 		return false;
 	}
 }
